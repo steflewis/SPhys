a272fd765ea4cc890e80b36cf9e50ff7db72f198
Stef Lewis <s.lewis.glasgow@gmail.com>
2012-03-15 13:53:05 +0000

    Adding cron job files

---
+++ threadtest_ocl_gpu_new.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+FILE=/home/Stefanie/NestedSampling/BeamRecoil/testme.out
+echo "Entering shell script" >>$FILE
+# flag=0
+# hour=17
+# min=52
+# #DIR=/home/Stefanie/NestedSampling/BeamRecoil/text_files/
+# for NTH in 1 2 4 8 16 32 64
+# do
+#     #scons dev=GPU impl=GPU iters=50000 data=${DIR}datatest_100000_1.txt nth=$NTH
+#     #FILE=/home/Stefanie/NestedSampling/BeamRecoil/times_ocl_gpu_test_100000_${NTH}thr.out
+#     #if [ -e $FILE ];
+#     #then
+#     #  echo "File $FILE exists"
+#     #  rm $FILE
+#     #fi
+#     
+# #     for (( c=1; c<=10; c++ ))
+# #     do
+# #       if [[ $(date +%H) -lt $hour || $(date +%M) -lt $min ]]; then
+# # 	flag=0
+# #       else
+# # #	  flag=1
+# #           echo Time is up.
+# # 	  exit
+# #       fi
+# # 	  
+# # 	  
+# # 	echo Hurray $NTH 
+# # 	sleep 5s
+# # #      { time ./usercode; } 2>>$FILE
+# #     done
+#      
+#     echo Done $NTH Threads
+#       
+# done
+#       
+      
