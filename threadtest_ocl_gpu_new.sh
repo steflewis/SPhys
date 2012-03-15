ace3db65dbad1ea60bb1e2e648a636cba7ee7b6e
Stef Lewis <s.lewis.glasgow@gmail.com>
2012-03-15 18:37:00 +0000

    Improving cron stuff

--- threadtest_ocl_gpu_new.sh
+++ threadtest_ocl_gpu_new.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 FILE=/home/Stefanie/NestedSampling/BeamRecoil/testme.out
 echo "Entering shell script" >>$FILE
-# flag=0
-# hour=17
-# min=52
+[3]<>$FILE
+
+#exec 3&>1
+
+flag=0
+hour=$((`date +%H`))
+min=$((`date +%M` + 2))
 # #DIR=/home/Stefanie/NestedSampling/BeamRecoil/text_files/
 # for NTH in 1 2 4 8 16 32 64
 # do
@@ -15,21 +19,25 @@ echo "Entering shell script" >>$FILE
 #     #  rm $FILE
 #     #fi
 #     
-# #     for (( c=1; c<=10; c++ ))
-# #     do
-# #       if [[ $(date +%H) -lt $hour || $(date +%M) -lt $min ]]; then
-# # 	flag=0
-# #       else
-# # #	  flag=1
-# #           echo Time is up.
-# # 	  exit
-# #       fi
-# # 	  
-# # 	  
-# # 	echo Hurray $NTH 
-# # 	sleep 5s
-# # #      { time ./usercode; } 2>>$FILE
-# #     done
+#     for (( c=1; c<=10; c++ ))
+#     do
+while [ $flag=0 ]; do
+  
+  
+  if [[ $(date +%H) -lt $hour || $(date +%M) -lt $min ]]; then
+    flag=0
+  else
+    flag=1
+    echo "Time is up." >>$FILE
+    exit
+  fi
+      # # 	  
+      # # 	  
+  echo "Hurray $flag" >>$FILE 
+  sleep 5s
+      # # #      { time ./usercode; } 2>>$FILE
+done
+      
 #      
 #     echo Done $NTH Threads
 #       
